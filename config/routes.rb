eda781912e9e7ca317bedcc2bf7fbc3e5fb0c800
Jake Singh <jasingh@ucdavis.edu>
2015-08-11 20:56:59 -0700

    Added simple question adding and viewing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root'
-  namespace :api do
-    resources :questions, :only => [:create, :index, :show]
-  end
+
   resources :users, :questions
   resource :session
 
