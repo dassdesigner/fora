e88d1696569281ebd4fc0020935754c9faf2219e
Jake Singh <jasingh@ucdavis.edu>
2015-08-11 20:59:56 -0700

    added api namespacing and question route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root'
-
+  namespace :api do
+    resources :questions, :only => [:create, :index, :show]
+  end
   resources :users, :questions
   resource :session
 
