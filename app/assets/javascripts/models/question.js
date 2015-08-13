e0488af0dec8246a831f121d0a076bdde83f1393
Jake Singh <jasingh@ucdavis.edu>
2015-08-13 10:12:14 -0700

    added answers backbone model

--- app/assets/javascripts/models/question.js
+++ app/assets/javascripts/models/question.js
@@ -1,3 +1,12 @@
 Fora.Models.Question = Backbone.Model.extend({
   urlRoot: "api/questions"
+  answers: function () {
+    if (!this._answers) {
+      this._answers = new Fora.Collections.Answers([], {question: this})
+    }
+
+    return this._cards;
+  },
+  
+  }
 });
