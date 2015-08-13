fb3466409bde493f738db2a09f7f2cabbbb7539e
Jake Singh <jasingh@ucdavis.edu>
2015-08-13 16:29:07 -0700

    added answer form view

---
+++ app/assets/javascripts/views/answers/answer_form.js
@@ -0,0 +1,26 @@
+Fora.Views.AnswerForm = Backbone.View.extend({
+  template: JST["answers/form"],
+  tagName: 'form',
+  events: {
+    "click button" : "submit"
+  },
+  render: function() {
+    var content = this.template({answer: this.model});
+    this.$el.html(content);
+    return this;
+  },
+
+  submit: function (event) {
+    event.preventDefault();
+    var attrs = this.$el.serializeJSON();
+    var that = this;
+
+    this.model.set(attrs);
+    this.model.save({}, {success: function () {
+      that.collection.add(that.model, {merge: true});
+      Backbone.history.navigate("", {trigger: true});
+      }
+    });
+  }
+
+});
