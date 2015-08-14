341b004faa09f71dbeea2eb8a7e887384f282978
Jake Singh <jasingh@ucdavis.edu>
2015-08-14 15:13:31 -0700

    added destroy dependencies

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
   validates :author_id, :title, :votes, presence: true
   belongs_to :user, foreign_key: :author_id
-  has_many :answers
+  has_many :answers, dependent: :destroy
 end
