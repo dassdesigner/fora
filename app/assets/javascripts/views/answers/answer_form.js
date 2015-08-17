4f6d2b124219514d14bde0393920715ea38242c5
Jake Singh <jasingh@ucdavis.edu>
2015-08-17 10:13:00 -0700

    added current username as global variable

--- app/assets/javascripts/views/answers/answer_form.js
+++ app/assets/javascripts/views/answers/answer_form.js
@@ -21,6 +21,7 @@ Fora.Views.AnswerForm = Backbone.View.extend({
     var attrs = this.$el.serializeJSON();
     var that = this;
     this.model.set(attrs);
+    this.model.set({answer_author_id: Fora.CURRENT_USER_ID});
     this.model.save({}, {success: function () {
       that.collection.add(that.model, {merge: true});
       Backbone.history.navigate("#questions/" + that.model.get('question_id'), {trigger: true});
