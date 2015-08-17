26e0792abdd5a549a9cd34b02f44d47fd4a65114
Jake Singh <jasingh@ucdavis.edu>
2015-08-17 15:59:07 -0700

    change to composite view

--- app/assets/javascripts/views/topics/topics_index.js
+++ app/assets/javascripts/views/topics/topics_index.js
@@ -3,7 +3,10 @@ Fora.Views.QuestionsIndex = Backbone.View.Extend({
 
   initialize: function () {
     this.listenTo(this.collection, "sync", this.render);
-  }
+    var that = this;
+    this.collection.each (function (topic) {
+      that.addTopicSubview(topic);
+      });
+    }
 
-  
 });
