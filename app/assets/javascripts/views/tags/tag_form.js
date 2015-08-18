a5f7b04ba5b7999dcb0c799a928e43980900d4e0
Jake Singh <jasingh@ucdavis.edu>
2015-08-18 14:27:24 -0700

    add tag form

---
+++ app/assets/javascripts/views/tags/tag_form.js
@@ -0,0 +1,32 @@
+Fora.Views.TagForm = Backbone.View.extend({
+  template: JST["tags/form"],
+  tagName: 'form',
+  className: 'tag-form',
+  events: {
+    "click button" : "submit"
+  },
+
+  initialize: function () {
+    this.listenTo(this.collection, "sync", this.render);
+  },
+
+  render: function() {
+    var content = this.template({tag: this.model});
+    this.$el.html(content);
+    return this;
+  },
+
+  submit: function (event) {
+    event.preventDefault();
+    var attrs = this.$el.serializeJSON();
+    var that = this;
+    this.model.set(attrs);
+    this.model.save({}, {success: function () {
+      that.collection.add(that.model, {merge: true});
+      // Backbone.history.navigate("#questions/" + that.model.get('question_id'), {trigger: true});
+      that.model = new Fora.Models.Tag();
+      }
+    });
+  }
+
+});
