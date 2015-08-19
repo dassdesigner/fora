574b0a1eb7c32586289c61adb7d3e054da0e7fdd
Jake Singh <jasingh@ucdavis.edu>
2015-08-19 15:17:04 -0700

    add get or fetch

--- app/assets/javascripts/collections/tags.js
+++ app/assets/javascripts/collections/tags.js
@@ -10,5 +10,22 @@ Fora.Collections.Tags = Backbone.Collection.extend({
 
   initialize: function (models, options) {
     if (options && options.question) { this.question = options.question; }
+  },
+
+  getOrFetch: function (id) {
+    var tag = this.get(id);
+    var that = this;
+
+    if (!tag) {
+      tag = new Fora.Models.Tag({id: id});
+      tag.fetch({
+        success: function(){
+          that.add(tag);
+        }
+      });
+    } else {
+      tag.fetch();
+    }
+    return tag;
   }
 });
