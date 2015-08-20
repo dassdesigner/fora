22012d40b43cce9f749bd9c5b23c6f5271d1c1f7
Jake Singh <jasingh@ucdavis.edu>
2015-08-19 17:48:13 -0700

    add method to return questions collection

--- app/assets/javascripts/models/tag.js
+++ app/assets/javascripts/models/tag.js
@@ -1,6 +1,13 @@
 Fora.Models.Tag = Backbone.Model.extend({
   urlRoot: 'api/tags',
 
+  questions: function () {
+    if (!this._questions) {
+      this._questions = new Fora.Collections.Questions([], {question: this});
+    }
+    return this.questions;
+  },
+
   getOrFetch: function (id) {
     var tag = this.get(id);
     var that = this;
