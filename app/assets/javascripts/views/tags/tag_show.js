107eeef1c6ecee758c309b9a905a25b18db906e5
Jake Singh <jasingh@ucdavis.edu>
2015-08-20 22:53:46 -0700

    remove extraneous commented out code

--- app/assets/javascripts/views/tags/tag_show.js
+++ app/assets/javascripts/views/tags/tag_show.js
@@ -14,16 +14,11 @@ Fora.Views.TagShow = Backbone.CompositeView.extend({
     this.listenTo(this.model, "sync", this.render);
     this.user_tags = options.user_tags;
     this.user_tags.fetch();
-    // this.listenTo(this.collection, "remove", this.removeQuestionSubview);
     debugger
     var that = this;
     this.collection.each (function (question) {
       that.addQuestionSubview(question);
     });
-
-    // this.tags.each (function (question) {
-    //   that.add
-    // })
   },
 
 
@@ -32,44 +27,25 @@ Fora.Views.TagShow = Backbone.CompositeView.extend({
     this.addSubview(".questions-container", subView);
   },
 
-  // removeQuestionSubview: function (question) {
-  //   that = this;
-  //   this.subviews('.answers-container').forEach(function(subview){
-  //     if (subview.model === answer) {
-  //       that.removeSubview('.answers-container', subview);
-  //     }
-  //   });
-  // },
-  // addTagsSubview: function (tag) {
-  //   var subView = new Fora.Views.TagsIndexItem({model: tag});
-  //   this.addSubview(".tags-container", subView);
-  // },
-
   render: function () {
     var content = this.template({tag: this.model});
     this.$el.html(content);
     if (this.user_tags.any( {id: this.model.get('id') } )) {
       this.$el.find('.follow').hide();
       this.$el.find('.unfollow').show();
-      // debugger
     } else {
-      // debugger
       this.$el.find('.follow').show();
       this.$el.find('.unfollow').hide();
     }
     this.attachSubviews();
-    // debugger
     return this;
   },
 
   follow: function (event) {
     event.preventDefault();
-    // this.$el.find('.follow').hide();
-    // this.$el.find('.unfollow').show();
     that = this;
     this.model.save({destroy: false}, {success: function () {
       that.user_tags.add(that.model, {merge: true});
-      // Backbone.history.navigate("#tags/" + that.model.get('id'), {trigger: true});
       }
     });
   },
@@ -79,7 +55,6 @@ Fora.Views.TagShow = Backbone.CompositeView.extend({
     that = this;
     this.model.save({destroy: true}, {success: function () {
       that.user_tags.remove(that.model);
-      // Backbone.history.navigate("#tags/" + that.model.get('id'), {trigger: true});
     }});
   }
 });
