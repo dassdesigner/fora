2274f95d4b9be958c9e5a32dd890d02fd9392853
Jake Singh <jasingh@ucdavis.edu>
2015-08-23 12:53:57 -0700

    add voteable mixin

--- app/assets/javascripts/models/answer.js
+++ app/assets/javascripts/models/answer.js
@@ -1,4 +1,14 @@
-Fora.Models.Answer = Backbone.Model.extend({
-  urlRoot: 'api/answers'
+Fora.Models.Answer = Backbone.Model.extend(
+  _.extend({}, Fora.Mixins.Voteable, {
+      urlRoot: 'api/answers',
 
-})
+      voteableOptions: {
+        foreignKey: "answer_id"
+      },
+
+      parse: function(payload) {
+        this.parseVote(payload);
+        return payload;
+      }
+    })
+);
