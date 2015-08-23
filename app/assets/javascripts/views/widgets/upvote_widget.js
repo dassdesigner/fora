080620cfe28099e26af28588c1aae63eb2413c5b
Jake Singh <jasingh@ucdavis.edu>
2015-08-23 16:21:23 -0700

    create upvote widget!

---
+++ app/assets/javascripts/views/widgets/upvote_widget.js
@@ -0,0 +1,23 @@
+Fora.Views.UpvoteWidget = Backbone.View.extend({
+  template: JST["widgets/upvote_widget"],
+
+  events: {
+    "click .upvote": "toggleUpvote"
+  },
+
+  initialize: function () {
+    this.listenTo(this.model, 'change:num_upvotes', this.render);
+  },
+
+  toggleUpvote: function(event) {
+    event.preventDefault();
+    this.model.toggleUpvote();
+  },
+
+  render: function() {
+    var content = this.template({
+      vote: this.model.vote(),
+      model: this.model
+    });
+  }
+});
