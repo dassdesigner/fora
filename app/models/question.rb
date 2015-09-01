d3bddf0e2b4d4d6d5643171332d03e7169c76ee9
Jake Singh <jasingh@ucdavis.edu>
2015-09-01 11:26:33 -0700

    add uniqueness validation for taggings

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,6 +6,7 @@ class Question < ActiveRecord::Base
   has_many :taggings
   has_many :votes, as: :voteable
   has_many :voters, :through => :votes, :source => :voter
+  validates_uniqueness_of :tags 
   def self.topic_matches(query)
     query_arr = query.split(" ")
     Question.all.select {|q| q.tags.any? { |tag| query_arr.any? { |query_word| tag.title.include?(query_word)}}}
--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,4 +1,5 @@
 class Tagging < ActiveRecord::Base
   belongs_to :question
   belongs_to :tag
+  validates_uniqueness_of :question_id, :scope => :tag_id
 end
