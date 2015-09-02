76f5bd0d05f2f028d3fc92c813bb6d3919cfbdd0
Jake Singh <jasingh@ucdavis.edu>
2015-09-02 12:41:36 -0700

    add template for follows

--- app/assets/javascripts/models/tag.js
+++ app/assets/javascripts/models/tag.js
@@ -1,21 +1,23 @@
 Fora.Models.Tag = Backbone.Model.extend(
   _.extend({}, Fora.Mixins.Followable, {
-  urlRoot: 'api/tags',
+    urlRoot: 'api/tags',
 
-  questions: function () {
-    if (!this._questions) {
-      this._questions = new Fora.Collections.Questions([], {question: this});
-    }
-    return this._questions;
-  },
+    questions: function() {
+      if (!this._questions) {
+        this._questions = new Fora.Collections.Questions([], {
+          question: this
+        });
+      }
+      return this._questions;
+    },
 
-  parse: function(payload) {
-    if (payload.questions) {
-      this.questions().set(payload.questions);
-      delete payload.questions;
+    parse: function(payload) {
+      if (payload.questions) {
+        this.questions().set(payload.questions);
+        delete payload.questions;
+      }
+      this.parseFollow(payload);
+      return payload;
     }
-    this.parseFollow(payload);
-    return payload;
-  }
-})
+  })
 );
