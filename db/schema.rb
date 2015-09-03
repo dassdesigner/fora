23a2b1a73e24e67852713c06626a5d8b8f59064b
Jake Singh <jasingh@ucdavis.edu>
2015-09-02 20:12:57 -0700

    add column for tag description

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823022102) do
+ActiveRecord::Schema.define(version: 20150903031132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,9 +53,10 @@ ActiveRecord::Schema.define(version: 20150823022102) do
   add_index "taggings", ["tag_id", "question_id"], name: "index_taggings_on_tag_id_and_question_id", using: :btree
 
   create_table "tags", force: :cascade do |t|
-    t.string   "title",      null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "title",       null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
   create_table "users", force: :cascade do |t|
