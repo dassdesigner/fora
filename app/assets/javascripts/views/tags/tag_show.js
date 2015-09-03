8b207b6fbb7f7447bf8e2667e6060d8290cfce01
Jake Singh <jasingh@ucdavis.edu>
2015-09-02 19:53:35 -0700

    remove old follow button

--- app/assets/javascripts/views/tags/tag_show.js
+++ app/assets/javascripts/views/tags/tag_show.js
@@ -1,10 +1,6 @@
 Fora.Views.TagShow = Backbone.CompositeView.extend({
   template: JST["tags/show"],
 
-  events: {
-    "click button.follow": "toggleFollow"
-  },
-
   //TODO refactor (it's pretty identical to QuestionsIndex)
   initialize: function (options) {
     this.userTags = options.user_tags;
@@ -29,47 +25,14 @@ Fora.Views.TagShow = Backbone.CompositeView.extend({
     this.addSubview(".content-container", subView);
   },
 
-  followString: function(){
-    return this.isFollowed() ? "Unfollow" : "Follow";
-  },
-  isFollowed: function(){
-    if (this.userTags.any( {id: this.model.get('id') } )) {
-      return true;
-    } else {
-      return false;
-    }
-  },
   render: function () {
     var content = this.template({
       tag: this.model,
-      followString: this.followString()
     });
     this.$el.html(content);
 
     this.attachSubviews();
     return this;
   },
-  //TODO change destroy to follow
-  toggleFollow: function(e){
-    e.preventDefault();
-    if(this.isFollowed()){
-      this.unfollow();
-    } else {
-      this.follow();
-    }
-  },
-  follow: function () {
-    that = this;
-    this.model.save({destroy: false}, {success: function () {
-      that.userTags.add(that.model, {merge: true});
-      }
-    });
-  },
 
-  unfollow: function () {
-    that = this;
-    this.model.save({destroy: true}, {success: function () {
-      that.userTags.remove(that.model);
-    }});
-  }
 });
