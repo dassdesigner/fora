17e7f940331327f904374b3ec176654b0db24991
Jake Singh <jasingh@ucdavis.edu>
2015-09-08 14:35:19 -0700

    fix answer formatting

--- app/assets/javascripts/views/tags/tags_index.js
+++ app/assets/javascripts/views/tags/tags_index.js
@@ -1,7 +1,7 @@
 Fora.Views.TagsIndex = Backbone.CompositeView.extend({
   template: JST["tags/index"],
   initialize: function () {
-    this.listenTo(this.collection, "sync", this.render);
+    // this.listenTo(this.collection, "sync", this.render);
     this.listenTo(this.collection, "add", this.addTagSubview);
     this.listenTo(this.collection, "remove", this.removeTagSubview);
     //TODO refactor into add all subviews
